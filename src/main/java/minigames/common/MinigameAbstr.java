973c0aad6b6ea96846f70d54360216bcee41d4b4
Emanuele Artegiani <emanuele.artegiani@studio.unibo.it>
2022-05-11 12:14:46 +0200

    Added playoff method

--- src/main/java/minigames/common/MinigameAbstr.java
+++ src/main/java/minigames/common/MinigameAbstr.java
@@ -1,10 +1,12 @@
 package minigames.common;
 
 import java.util.List;
+import java.util.Map;
 
 public abstract class MinigameAbstr<U> implements Minigame<U> {
 
 	private final List<U> players;
+	private Map<U, Integer> playersClassification;
 
 	public MinigameAbstr(final List<U> players) {
 		this.players = players;
@@ -12,16 +14,21 @@ public abstract class MinigameAbstr<U> implements Minigame<U> {
 
 	@Override
 	public List<U> runGame() {
-		return null;
-	}
-
-	@Override
-	public List<U> playoff() {
-		return null;
+		
+		return this.gameResults();
 	}
 
 	@Override
 	public List<U> gameResults() {
+		
+		return null;
+	}
+	
+	/**
+     * This method manages the draws at the end of the minigame
+     * @return a list of players ordered by their classification in the minigame and draws already managed
+	 */
+	private List<U> playoff() {
 		return null;
 	}
 	
