3e20a8e8e85f1362b7c2219b3e24dbf79a61fffb
Emanuele Artegiani <emanuele.artegiani@studio.unibo.it>
2022-05-11 14:35:18 +0200

    Created TestMinigame

---
+++ src/test/java/minigames/common/TestMinigame.java
@@ -0,0 +1,22 @@
+package minigames.common;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+class TestMinigame {
+
+	@Test
+	void testScoreMapper() {
+		
+	}
+
+	
+	@Test
+	void testSortPlayerByScore() {
+		
+	}
+
+}
