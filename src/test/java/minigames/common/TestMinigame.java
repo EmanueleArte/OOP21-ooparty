26abb799b26888f35844a47f4c43f54c627292ec
Emanuele Artegiani <emanuele.artegiani@studio.unibo.it>
2022-05-11 15:16:48 +0200

    Added testScoreMapper

--- src/test/java/minigames/common/TestMinigame.java
+++ src/test/java/minigames/common/TestMinigame.java
@@ -2,7 +2,9 @@ package minigames.common;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.junit.jupiter.api.Test;
 
@@ -10,7 +12,22 @@ class TestMinigame {
 
 	@Test
 	void testScoreMapper() {
+		List<String> players = List.of("Luca", "Giovanni", "Lorenzo", "Marco");
+		List<Integer> scores = List.of(4, 7, 5, 2);
 		
+		class MinigameImpl<U> extends MinigameAbstr<U> {
+
+			public MinigameImpl(List<U> players) {
+				super(players);
+			}
+			
+		}
+		
+		MinigameImpl<String> m = new MinigameImpl<String>(players);
+		players.forEach(p -> m.scoreMapper(p, scores.get(players.indexOf(p))));
+		Map<String, Integer> correctMap = new HashMap<>();
+		players.forEach(p -> correctMap.put(p, scores.get(players.indexOf(p))));
+		assertEquals(correctMap, m.getPlayersClassification());
 	}
 
 	
