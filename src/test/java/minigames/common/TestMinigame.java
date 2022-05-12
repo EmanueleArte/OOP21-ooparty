5549f978aa2a268fb5675097fb029b0c5c3501a5
Emanuele Artegiani <emanuele.artegiani@studio.unibo.it>
2022-05-12 08:19:44 +0200

    Minor test fixes

--- src/test/java/minigames/common/TestMinigame.java
+++ src/test/java/minigames/common/TestMinigame.java
@@ -13,8 +13,7 @@ class TestMinigame {
 
 	@Test
 	void testScoreMapper() {
-		List<Optional<String>> players = List.of(Optional.of("Luca"), Optional.of("Giovanni"), 
-				Optional.of("Lorenzo"), Optional.of("Marco"));
+		List<String> players = List.of("Luca", "Giovanni", "Lorenzo", "Marco");
 		List<Integer> scores = List.of(4, 7, 5, 2);
 		
 		class MinigameImpl<U> extends MinigameAbstr<U> {
@@ -22,13 +21,17 @@ class TestMinigame {
 			public MinigameImpl(final List<U> players) {
 				super(players);
 			}
+
+			@Override
+			public List<U> runGame() {
+				return null;
+			}
 			
 		}
 		
-		MinigameImpl<Optional<String>> m = new MinigameImpl<Optional<String>>(players);
+		MinigameImpl<String> m = new MinigameImpl<String>(players);
 		players.forEach(p -> m.scoreMapper(p, scores.get(players.indexOf(p))));
-		Map<Optional<String>, Integer> correctMap = Map.of(Optional.of("Luca"), 4, Optional.of("Giovanni"), 7, 
-				Optional.of("Lorenzo"), 5, Optional.of("Marco"), 2);
+		Map<String, Integer> correctMap = Map.of("Luca", 4, "Giovanni", 7, "Lorenzo", 5, "Marco", 2);
 		assertEquals(correctMap, m.getPlayersClassification());
 	}
 
@@ -40,15 +43,18 @@ class TestMinigame {
 			public MinigameImpl() {
 				super();
 			}
+
+			@Override
+			public List<U> runGame() {
+				return null;
+			}
 			
 		}
 		
-		MinigameImpl<Optional<String>> m = new MinigameImpl<Optional<String>>();
-		Map<Optional<String>, Integer> simpleMap = Map.of(Optional.of("Luca"), 4, Optional.of("Giovanni"), 7, 
-				Optional.of("Lorenzo"), 5, Optional.of("Marco"), 2);
+		MinigameImpl<String> m = new MinigameImpl<String>();
+		Map<String, Integer> simpleMap = Map.of("Luca", 4, "Giovanni", 7, "Lorenzo", 5, "Marco", 2);
 		m.setPlayersClassification(simpleMap);
-		List<Optional<String>> orderedList = List.of(Optional.of("Giovanni"), Optional.of("Lorenzo"), 
-				Optional.of("Luca"), Optional.of("Marco"));
+		List<String> orderedList = List.of("Giovanni", "Lorenzo", "Luca", "Marco");
 		assertEquals(orderedList, m.gameResults());
 		/*Map<Optional<String>, Integer> duplMap = Map.of(Optional.of("Luca"), 2, Optional.of("Giovanni"), 7, 
 				Optional.of("Lorenzo"), 2, Optional.of("Marco"), 6);
