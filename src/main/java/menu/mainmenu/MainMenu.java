ec8f95a075f55e64d733808c23c257bfbc6d30e6
Emanuele Artegiani <emanuele.artegiani@studio.unibo.it>
2022-06-27 11:24:33 +0200

    Added main menu classes

--- src/main/java/menu/mainmenu/MainMenu.java
+++ src/main/java/menu/mainmenu/MainMenu.java
@@ -4,5 +4,14 @@ package menu.mainmenu;
  * This interface models the main menu of the game.
  */
 public interface MainMenu {
-
+	
+	/**
+	 * This method exits from the game.
+	 */
+	void exitGame();
+	
+	/**
+	 * This method create a new game.
+	 */
+	void createGame();
 }
---
+++ src/main/java/menu/mainmenu/MainMenuImpl.java
@@ -0,0 +1,21 @@
+package menu.mainmenu;
+
+public class MainMenuImpl implements MainMenu {
+
+	public MainMenuImpl() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void exitGame() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void createGame() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
