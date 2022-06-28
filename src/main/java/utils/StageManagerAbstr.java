ac361631bd44ae474e740461e5147268c175b3bb
Emanuele Artegiani <emanuele.artegiani@studio.unibo.it>
2022-06-28 13:26:49 +0200

    Add StageManager abstract class

---
+++ src/main/java/utils/StageManagerAbstr.java
@@ -0,0 +1,42 @@
+package utils;
+
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.application.Application;
+
+public class StageManagerAbstr extends Application implements StageManager {
+
+	public StageManagerAbstr() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void addScene() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Scene popScene() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setScene() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void run(String[] args) {
+		launch();
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
