08fba8c08df3f88dbc2d1eeadb8b5e1590d3ba7f
Emanuele Artegiani <emanuele.artegiani@studio.unibo.it>
2022-06-28 11:28:17 +0200

    Fix main menu view

--- src/main/java/menu/mainmenu/view/MainMenuViewImpl.java
+++ src/main/java/menu/mainmenu/view/MainMenuViewImpl.java
@@ -2,18 +2,13 @@ package menu.mainmenu.view;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Insets;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class MainMenuViewImpl extends Application implements MainMenuView {
 
 	private Stage primaryStage;
-	static final Font fontButton = new Font(50);
-	static final Font fontTitle = new Font(70);
-	static final Insets insets = new Insets(40);
 	
 	public MainMenuViewImpl() {
 		super();
