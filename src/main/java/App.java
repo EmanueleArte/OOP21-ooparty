838146b40ea761be967c0f5872a39741e01a3ee8
Roberto Draghetti <roberto.draghetti@studio.unibo.it>
2022-06-29 19:35:48 +0200

    add diceModel

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,3 @@
-import java.util.List;
-
-import game.dice.model.DiceModel;
-import game.dice.model.DiceModelImpl;
 import javafx.scene.Scene;
 import menu.mainmenu.view.MainMenuView;
 import menu.mainmenu.view.MainMenuViewImpl;
@@ -20,15 +16,6 @@ public class App {
         stageManager.run();
         final MainMenuView<Scene> mainMenu = new MainMenuViewImpl<>(stageManager);
         mainMenu.createMainMenu();
-        
-        DiceModel dice = new DiceModelImpl();
-        for(int i=0;i<10;i++) {
-        	System.out.println("\nRisultato: "+dice.rollDice(null));
-        	System.out.println("Spareggio: ");
-        	dice.rollDices(List.of(1, 2, 3, 4)).forEach(r->{
-        		System.out.print(" "+r);
-        	});
-        }
     }
     
 }
