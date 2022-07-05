f82c987812dffef6e0c4861e7504965e278c8e7b
Emanuele Artegiani <emanuele.artegiani@studio.unibo.it>
2022-07-05 11:32:06 +0200

    Minor fixes

--- src/main/java/minigames/mastermind/control/MastermindControllerImpl.java
+++ src/main/java/minigames/mastermind/control/MastermindControllerImpl.java
@@ -1,10 +1,20 @@
 package minigames.mastermind.control;
 
+import java.util.List;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import minigames.common.model.MinigameModel;
+
 /**
  * Implementation of {@link MastermindController}.
  */
 public class MastermindControllerImpl<S, U> implements MastermindController<S, U> {
 
+	private MinigameModel<S, U> mastermindModel;
+	@FXML private List<Label> playersNicknames;
+	@FXML private TextField inputField;
+	
 	public MastermindControllerImpl() {
 		// TODO Auto-generated constructor stub
 	}
--- src/main/java/minigames/mastermind/model/MastermindModelImpl.java
+++ src/main/java/minigames/mastermind/model/MastermindModelImpl.java
@@ -2,15 +2,16 @@ package minigames.mastermind.model;
 
 import java.util.List;
 
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import minigames.common.model.MinigameModelAbstr;
 import utils.graphics.StageManager;
 
 public class MastermindModelImpl<S, U> extends MinigameModelAbstr<S, U> {
 
-	private final List<TextField> attempts;
+	private final List<Label> attempts;
 	
-	public MastermindModelImpl(final List<U> players, final StageManager<S> s, final List<TextField> attempts) {
+	public MastermindModelImpl(final List<U> players, final StageManager<S> s, final List<Label> attempts) {
 		super(players, s);
 		this.attempts = attempts;
 	}
