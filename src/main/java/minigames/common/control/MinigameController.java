d8d12c82875a72f0d02a28ad8c2f825d0784f76c
Emanuele Artegiani <emanuele.artegiani@studio.unibo.it>
2022-07-19 17:32:15 +0200

    Fix minigame controller

--- src/main/java/minigames/common/control/MinigameController.java
+++ src/main/java/minigames/common/control/MinigameController.java
@@ -5,16 +5,14 @@ import java.util.List;
 /**
  * This interface models a minigame controller.
  * 
- * @param <S> the scenes of the stage
- * @param <U> the {@link game.player.Player}
  */
-public interface MinigameController<S, U> {
+public interface MinigameController {
 
     /**
      * This method returns the results of the minigame.
      * 
      * @return the list of players ordered by their score in the minigame (from higher to lower)
      */
-    List<U> getGameResults();
+    <U> List<U> getGameResults();
 
 }
