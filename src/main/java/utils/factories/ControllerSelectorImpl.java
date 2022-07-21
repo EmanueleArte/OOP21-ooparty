4c127c720e2c7b9324e078724a66dd49a649868e
Emanuele Artegiani <emanuele.artegiani@studio.unibo.it>
2022-07-21 09:47:30 +0200

    Add selectControlleraCallback method

--- src/main/java/utils/factories/ControllerSelectorImpl.java
+++ src/main/java/utils/factories/ControllerSelectorImpl.java
@@ -13,16 +13,30 @@ import utils.graphics.StageManager;
  */
 public class ControllerSelectorImpl<S> implements ControllerSelector<S> {
 
-    final private ControllerFactory<S> controlFactory;
+    private final ControllerFactory<S> controlFactory;
 
+    /**
+     * Builds a new {@link ControllerSelectorImpl}.
+     * 
+     * @param s the {@link utils.graphics.StageManager}
+     */
     public ControllerSelectorImpl(final StageManager<S> s) {
         this.controlFactory = new ControllerFactoryImpl<>(s);
     }
 
     @Override
-    public final <U> Callback<Class<?>, Object> selectControllerCallback(final ControllerType controller, final List<U> players) {
-        // TODO Auto-generated method stub
-        return null;
+    public final <U> Callback<Class<?>, Object> selectControllerCallback(final ControllerType controller,
+            final List<U> players) {
+        switch (controller) {
+        case MAIN_MENU:
+            return this.controlFactory.createMainMenuController();
+        case GAME_CREATION_MENU:
+            return this.controlFactory.createGameCreationMenuController();
+        case MASTERMIND:
+            return this.controlFactory.createMastermind(players);
+        default:
+            return null;
+        }
     }
 
 }
