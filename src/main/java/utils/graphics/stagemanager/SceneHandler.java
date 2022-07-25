5c8e598783a4ae7eb24e43598037b83f089a0fa5
Emanuele Artegiani <emanuele.artegiani@studio.unibo.it>
2022-07-25 19:02:01 +0200

    Add SceneHandler class

---
+++ src/main/java/utils/graphics/stagemanager/SceneHandler.java
@@ -0,0 +1,14 @@
+package utils.graphics.stagemanager;
+
+/**
+ * This class models the actions that handle the scenes.
+ * 
+ * @param <S> the scenes of the stage
+ */
+public class SceneHandler<S> {
+
+    public SceneHandler() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
