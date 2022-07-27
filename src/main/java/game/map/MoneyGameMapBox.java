a2fd26c8cb180fa9f3c3243f659d5d550b52914f
Lorenzo Guerrini <lorenzo.guerrini5@studio.unibo.it>
2022-07-27 10:08:45 +0200

    Added method to receive money from a map box

--- src/main/java/game/map/MoneyGameMapBox.java
+++ src/main/java/game/map/MoneyGameMapBox.java
@@ -2,15 +2,26 @@ package game.map;
 
 import java.util.Random;
 
+import game.player.Player;
+
 public class MoneyGameMapBox extends GameMapBoxImpl {
     private int moneyNumber;
 
     public MoneyGameMapBox() {
         super();
-        this.moneyNumber = new Random().nextInt(GameMapBoxImpl.MAX_MONEY);
+        this.generateNewMoney();
     }
 
     public int getMoneyNumber() {
-        return moneyNumber;
+        return this.moneyNumber;
+    }
+
+    public void receiveMoney(final Player p) {
+        p.updateCoins(this.moneyNumber);
+        this.generateNewMoney();
+    }
+
+    private void generateNewMoney() {
+        this.moneyNumber = new Random().nextInt(GameMapBoxImpl.MAX_MONEY); 
     }
 }
