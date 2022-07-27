13ab4598f912aa602b3050a90e14773e446231fc
Emanuele Artegiani <emanuele.artegiani@studio.unibo.it>
2022-07-27 16:47:36 +0200

    Add javadoc

--- src/main/java/minigames/mastermind/viewcontroller/MastermindViewController.java
+++ src/main/java/minigames/mastermind/viewcontroller/MastermindViewController.java
@@ -13,6 +13,12 @@ public interface MastermindViewController extends MinigameViewController {
 
     /**
      * This method shows the results of the turn.
+     * 
+     * @param win       is true if the player has guessed
+     * @param lose      is true if the player ended the turns without guessing
+     * @param score     the score of the player
+     * @param solution  the 4-digit number to guess
+     * @param nAttempts the number of attempts done
      */
     void showTurnResults(boolean win, boolean lose, int score, String solution, int nAttempts);
 
