49e46253851a970dcd9ee22ce0f91580b3439f62
Emanuele Artegiani <emanuele.artegiani@studio.unibo.it>
2022-07-28 14:33:38 +0200

    Fix constructor

--- src/main/java/utils/factories/GenericControllerCallback.java
+++ src/main/java/utils/factories/GenericControllerCallback.java
@@ -1,7 +1,6 @@
 package utils.factories;
 
 import java.lang.reflect.InvocationTargetException;
-import java.util.List;
 import javafx.util.Callback;
 
 /**
@@ -14,24 +13,13 @@ class GenericControllerCallback<U> implements Callback<Class<?>, Object> {
     private final Class<?> controllerClass;
 
     /**
-     * Builds a new {@link GenericControllerCallback}.
-     * 
-     * @param players         the list of players
-     * @param controllerClass the class of the controller
-     */
-    GenericControllerCallback(final List<U> players, final Class<?> controllerClass) {
-        super();
-        this.controllerClass = controllerClass;
-    }
-
-    /**
      * Builds a new {@link GenericControllerCallback} with no players.
      * 
      * @param s               the {@link utils.graphics.stagemanager.StageManager}
      * @param controllerClass the class of the controller
      */
     GenericControllerCallback(final Class<?> controllerClass) {
-        this(null, controllerClass);
+        this.controllerClass = controllerClass;
     }
 
     @Override
