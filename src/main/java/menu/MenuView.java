ad229fd0e928ac61aefb676c89c0a2d7ba25a2db
Emanuele Artegiani <emanuele.artegiani@studio.unibo.it>
2022-07-29 11:05:16 +0200

    Add MenuView interface

---
+++ src/main/java/menu/MenuView.java
@@ -0,0 +1,19 @@
+package menu;
+
+import utils.GenericController;
+
+/**
+ * This interface models the creation menu view of the game.
+ * 
+ * @param <S> the scenes of the stage
+ */
+public interface MenuView<S> {
+
+    /**
+     * This method shows a menu into the javafx gui.
+     * 
+     * @param controller the controller to be used
+     */
+    void createMenu(GenericController controller);
+
+}
