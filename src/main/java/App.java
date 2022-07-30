6811d45762c3a3be5cb6ab86c4588506d04cc30b
Lorenzo Guerrini <lorenzo.guerrini5@studio.unibo.it>
2022-07-30 15:55:37 +0200

    Fixed App

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,9 +8,6 @@ import game.player.PlayerImpl;
 import javafx.scene.Scene;
 import menu.MenuController;
 import menu.mainmenu.controller.MainMenuControllerImpl;
-import minigames.whoriskswins.controller.WhoRisksWinsController;
-import minigames.whoriskswins.controller.WhoRisksWinsControllerImpl;
-import utils.enums.PlayerColor;
 import utils.graphics.stagemanager.StageManager;
 import utils.graphics.stagemanager.StageManagerImpl;
 
@@ -24,14 +21,9 @@ public final class App {
     public static void main(final String[] args) {
         final StageManager<Scene> stageManager = new StageManagerImpl<>("OOparty");
         stageManager.run();
-<<<<<<< HEAD
         final MenuController mainMenu = new MainMenuControllerImpl(stageManager);
         mainMenu.createMenu();
-=======
-        final MainMenuView<Scene> mainMenu = new MainMenuViewImpl<>(stageManager);
-        mainMenu.createMainMenu();
         final GameMap gameMap = new GameMapImpl(null);
->>>>>>> map
 
         GameHandler game = new GameHandlerImpl(stageManager,
                 List.of(new PlayerImpl("Mario"), new PlayerImpl("Luigi"), new PlayerImpl("Yoshi")), gameMap);
