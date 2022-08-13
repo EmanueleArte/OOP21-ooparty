6c12eaa40b4f9dc6d4c4fcbfec111bbb3d323f25
Emanuele Artegiani <emanuele.artegiani@studio.unibo.it>
2022-08-13 12:33:31 +0200

    Minor fix

--- src/main/java/utils/view/GenericViewUtils.java
+++ src/main/java/utils/view/GenericViewUtils.java
@@ -4,7 +4,8 @@ import utils.controller.GenericController;
 import utils.graphics.stagemanager.StageManager;
 
 /**
- * The class {@link GenericViewUtils} contains a method useful to create a scene using javafx.
+ * The class {@link GenericViewUtils} contains a method useful to create a scene
+ * using javafx.
  */
 public final class GenericViewUtils {
 
@@ -13,12 +14,15 @@ public final class GenericViewUtils {
 
     /**
      * This method create a specific scene given the arguments.
-     * @param stageManager - the stage manager of the application
-     * @param controller - the controller that act on the scene
-     * @param viewControllerClass - the class of the viewcontroller that handle the scene
-     * @param fxmlUrl - the url to the fxml file to open
+     * 
+     * @param stageManager        the stage manager of the application
+     * @param controller          the controller that act on the scene
+     * @param viewControllerClass the class of the viewcontroller that handle the
+     *                            scene
+     * @param fxmlUrl             the url to the fxml file to open
      */
-    public static void createScene(final StageManager stageManager, final GenericController controller, final Class<?> viewControllerClass, final String fxmlUrl) {
+    public static <S> void createScene(final StageManager<S> stageManager, final GenericController controller,
+            final Class<?> viewControllerClass, final String fxmlUrl) {
         stageManager.addFXMLScene(fxmlUrl, viewControllerClass, controller);
     }
 
