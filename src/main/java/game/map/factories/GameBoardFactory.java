ead29f7601120fdb90f239566099ed26bf07ffb1
Enrico Tagliaferri <enrico.tagliaferri2@studio.unibo.it>
2022-08-16 08:52:05 +0200

    Minor fix

--- src/main/java/game/map/factories/GameBoardFactory.java
+++ src/main/java/game/map/factories/GameBoardFactory.java
@@ -8,18 +8,18 @@ import game.map.GameMapSquare;
  * An interface modeling a factory for the board game.
  */
 public interface GameBoardFactory {
-	
-	/**
-	 * This method returns a list of {@link GameMapSquare} that represents the game board.
-	 * @return a list representing the game board.
-	 */
-	List<GameMapSquare> createGameBoard();
-	
-	/**
-	 * This method returns a list (of length size) of {@link GameMapSquare} that represents the game board.
-	 * @param the size of the board
-	 * @return a list representing the game board
-	 */
-	List<GameMapSquare> createGameBoard(int size);
-	
+
+        /**
+         * This method returns a list of {@link GameMapSquare} that represents the game board.
+         * @return a list representing the game board.
+         */
+        List<GameMapSquare> createGameBoard();
+
+        /**
+         * This method returns a list (of length size) of {@link GameMapSquare} that represents the game board.
+         * @param the size of the board
+         * @return a list representing the game board
+         */
+        List<GameMapSquare> createGameBoard(int size);
+
 }
