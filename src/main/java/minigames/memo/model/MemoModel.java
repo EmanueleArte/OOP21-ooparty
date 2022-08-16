1a26f6b3484f6406ec8c1f04b8d5df3672c611fd
Leonardo Beleffi <leonardo.beleffi@studio.unibo.it>
2022-08-16 20:45:05 +0200

    Decorative fix
     - Format code

--- src/main/java/minigames/memo/model/MemoModel.java
+++ src/main/java/minigames/memo/model/MemoModel.java
@@ -3,16 +3,17 @@ package minigames.memo.model;
 import minigames.common.model.MinigameModel;
 
 /**
- * This interface models the minigame {@code memo}'s model.
- * This interface is a specialization of {@link MinigameModel}
+ * This interface models the minigame {@code memo}'s model. This interface is a
+ * specialization of {@link MinigameModel}
  *
  * @param <S> the scenes of the stage
  */
 public interface MemoModel<S> extends MinigameModel<S> {
-	
-	boolean isOver();
-	
-	int chooseCard(final int index) ;
-	
-	void changeTurn();
+
+    boolean isOver();
+
+    boolean chooseCards(int indexFirstCard, int indexSecondCard);
+
+    void changeTurn();
+
 }
