c793f23277f8f68d5c79d41ea26da2d3bd0d166f
Leonardo Beleffi <leonardo.beleffi@studio.unibo.it>
2022-08-17 16:10:44 +0200

    Add draft for MemoController in minigames

---
+++ src/main/java/minigames/memo/controller/MemoController.java
@@ -0,0 +1,12 @@
+package minigames.memo.controller;
+
+import minigames.common.controller.MinigameController;
+
+/**
+ * This interface models the minigame {@code memo}'s controller. This interface is a
+ * specialization of {@link MinigameController}.
+ *
+ */
+public interface MemoController extends MinigameController {
+
+}
