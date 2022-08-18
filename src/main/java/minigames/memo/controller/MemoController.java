7c35f6abd51e02209bfec398049a705f09691bd0
Leonardo Beleffi <leonardo.beleffi@studio.unibo.it>
2022-08-18 10:32:37 +0200

    Decorative fix
     - Remove comment

--- src/main/java/minigames/memo/controller/MemoController.java
+++ src/main/java/minigames/memo/controller/MemoController.java
@@ -5,7 +5,6 @@ import minigames.common.controller.MinigameController;
 /**
  * This interface models the minigame {@code memo}'s controller. This interface is a
  * specialization of {@link MinigameController}.
- *
  */
 public interface MemoController extends MinigameController {
 
