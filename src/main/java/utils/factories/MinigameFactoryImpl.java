f632db19a2fd1a179bb1e545991152599c9444a4
Leonardo Beleffi <leonardo.beleffi@studio.unibo.it>
2022-08-18 12:27:24 +0200

    Set to always return memo *for debug*

--- src/main/java/utils/factories/MinigameFactoryImpl.java
+++ src/main/java/utils/factories/MinigameFactoryImpl.java
@@ -6,11 +6,12 @@ import java.util.Random;
 import game.player.Player;
 import minigames.common.controller.MinigameController;
 import minigames.mastermind.controller.MastermindControllerImpl;
+import minigames.memo.controller.MemoControllerImpl;
 import minigames.whoriskswins.controller.WhoRisksWinsControllerImpl;
 import utils.graphics.controller.StageManager;
 
 public class MinigameFactoryImpl<S> {
-    private static final int MINIGAMES_NUMBER = 2;
+    private static final int MINIGAMES_NUMBER = 3;
 
     private final StageManager<S> stageManager;
     private final List<Player> players;
@@ -21,15 +22,20 @@ public class MinigameFactoryImpl<S> {
     }
 
     public final MinigameController createRandomMinigameController() {
+        /*
         Random rand = new Random();
         switch (rand.nextInt(MinigameFactoryImpl.MINIGAMES_NUMBER)) {
         case 0:
             return this.createMastermindController();
         case 1:
             return this.createWhoRisksWinsController();
+        case 2:
+            return this.createMemoController();
         default:
             return null;
         }
+        */
+        return this.createMemoController();
     }
 
     public final MinigameController createMastermindController() {
@@ -38,5 +44,9 @@ public class MinigameFactoryImpl<S> {
 
     public final MinigameController createWhoRisksWinsController() {
         return new WhoRisksWinsControllerImpl(this.stageManager, this.players);
+    }
+
+    public final MinigameController createMemoController() {
+        return new MemoControllerImpl(this.stageManager, this.players);
     }
 }
