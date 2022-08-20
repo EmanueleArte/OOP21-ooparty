c3625133713c8ed1211cba7a3acafc105a116b7c
Lorenzo Guerrini <lorenzo.guerrini5@studio.unibo.it>
2022-08-20 12:03:40 +0200

    Fixed after minigame menu controller creation

--- src/main/java/game/gamehandler/controller/GameHandlerController.java
+++ src/main/java/game/gamehandler/controller/GameHandlerController.java
@@ -66,4 +66,9 @@ public interface GameHandlerController {
     void checkPlayerDeath(Player p);
 
     List<Player> getTurnOrder();
+
+    /**
+     * Shows the after minigame menu.
+     */
+    void showAfterMinigameMenu();
 }
