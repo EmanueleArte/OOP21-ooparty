c2901dfe432be3139e9f7490248638599d01a619
Enrico Tagliaferri <enrico.tagliaferri2@studio.unibo.it>
2022-08-21 15:29:38 +0200

    Minor fix

--- src/test/java/game/TestLeaderboard.java
+++ src/test/java/game/TestLeaderboard.java
@@ -1,35 +1,30 @@
 package game;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import java.util.List;
-
 import org.junit.jupiter.api.Test;
+import game.dice.model.DiceModel;
+import game.dice.model.DiceModelImpl;
 import game.gamehandler.model.GameHandlerModel;
 import game.gamehandler.model.GameHandlerModelImpl;
-import game.map.GameMap;
-import game.map.GameMapImpl;
 import game.player.Player;
 import game.player.PlayerImpl;
-import utils.graphics.controller.StageManager;
-import utils.graphics.controller.StageManagerImpl;
 
 class TestLeaderboard {
 
     @Test
     void startingLeaderboardTest() {
-        final StageManager<?> stageManager = new StageManagerImpl<>("Test", null /*TODO non va così ma mancava un argomento prima*/);
+        final DiceModel dice = new DiceModelImpl();
         final List<Player> players = List.of(new PlayerImpl("Mario"), new PlayerImpl("Luigi"), new PlayerImpl("Alex"));
         final int turns = 5;
-        final GameMap map = new GameMapImpl();
-        final GameHandlerModel model = new GameHandlerModelImpl(stageManager, players, turns, map);
+        final GameHandlerModel model = new GameHandlerModelImpl(dice, players, turns);
 
         assertEquals(players, model.getLeaderboard());
     }
 
     @Test
     void midGameLeaderboardTest() {
-        final StageManager<?> stageManager = new StageManagerImpl<>("Test", null /*TODO non va così ma mancava un argomento prima*/);
+        final DiceModel dice = new DiceModelImpl();
 
         final Player mario = new PlayerImpl("Mario");
         final Player luigi = new PlayerImpl("Luigi");
@@ -38,8 +33,7 @@ class TestLeaderboard {
 
         final List<Player> players = List.of(mario, luigi, alex, martino);
         final int turns = 5;
-        final GameMap map = new GameMapImpl();
-        final GameHandlerModel model = new GameHandlerModelImpl(stageManager, players, turns, map);
+        final GameHandlerModel model = new GameHandlerModelImpl(dice, players, turns);
 
         model.getPlayers().get(0).earnCoins(20);
         model.getPlayers().get(1).earnCoins(50);
