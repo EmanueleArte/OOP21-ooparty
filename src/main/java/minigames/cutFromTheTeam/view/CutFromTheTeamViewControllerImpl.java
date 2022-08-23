61bf7667ac815327a32f702309d41bb625608e70
Leonardo Beleffi <leonardo.beleffi@studio.unibo.it>
2022-08-23 15:01:12 +0200

    Add draft for viewImpl in CutFromTheRope

---
+++ src/main/java/minigames/cutFromTheTeam/view/CutFromTheTeamViewControllerImpl.java
@@ -0,0 +1,41 @@
+package minigames.cutFromTheTeam.view;
+
+import java.util.List;
+
+import game.player.Player;
+import javafx.scene.input.KeyEvent;
+import minigames.common.view.MinigameViewControllerAbstr;
+import utils.controller.GenericController;
+
+/**
+ * Implementation of {@link CutFromTheTeamViewController} and extension of
+ * {@link MinigameViewControllerAbstr}.
+ */
+public class CutFromTheTeamViewControllerImpl extends MinigameViewControllerAbstr
+        implements CutFromTheTeamViewController {
+
+    @Override
+    public void startNextTurn() {
+    }
+
+    @Override
+    public void setPlayerLabelText(final Player player) {
+    }
+
+    @Override
+    public void setPlayerAvatarColor(final Player player) {
+    }
+
+    @Override
+    public void setController(final GenericController controller) throws IllegalArgumentException {
+    }
+
+    @Override
+    protected void onEnter(final KeyEvent ke) {
+    }
+
+    @Override
+    public void start(final List<Boolean> ropes) {
+    }
+
+}
