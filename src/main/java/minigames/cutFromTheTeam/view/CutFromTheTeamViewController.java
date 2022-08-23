9043d1f216becd3d125e5566c17a31bf113b40b8
Leonardo Beleffi <leonardo.beleffi@studio.unibo.it>
2022-08-23 15:05:53 +0200

    Add method to start the view

--- src/main/java/minigames/cutFromTheTeam/view/CutFromTheTeamViewController.java
+++ src/main/java/minigames/cutFromTheTeam/view/CutFromTheTeamViewController.java
@@ -1,11 +1,21 @@
 package minigames.cutFromTheTeam.view;
 
+import java.util.List;
+
 import minigames.common.view.MinigameViewController;
 
 /**
- * This interface models the minigame {@code cut from the team}'s view. This interface
- * is a specialization of {@link MinigameViewController}.
+ * This interface models the minigame {@code cut from the team}'s view. This
+ * interface is a specialization of {@link MinigameViewController}.
  */
 public interface CutFromTheTeamViewController extends MinigameViewController {
+
+    /**
+     * This method acts as the constructor for the class
+     * {@link CutFromTheTeamViewControllerImpl}. It maps the buttons to their value.
+     *
+     * @param ropes the number of buttons to keep.
+     */
+    void start(List<Boolean> ropes);
 
 }
