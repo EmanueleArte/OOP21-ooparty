c017de4e61d2b59f5c4354900c45a8475d985d79
Enrico Tagliaferri <enrico.tagliaferri2@studio.unibo.it>
2022-08-23 09:20:25 +0200

    Add javadoc

--- src/main/java/utils/readers/MapLayoutReader.java
+++ src/main/java/utils/readers/MapLayoutReader.java
@@ -11,8 +11,8 @@ public interface MapLayoutReader {
     /**
      * Loads the specified {@link MapLayout}.
      * @param layoutType  the type of map to load
-     * @return a list of {@link Point2D} where each element represent the position of a {@link GameMapSquare}
-     * @throws IOException 
+     * @return a list of {@link Point2D} where each element represent the position of a {@link game.map.GameMapSquare GameMapSquare}
+     * @throws IOException if an error occurs while reading the layout file
      */
     List<Point2D> loadMapLayout(MapLayout layoutType) throws IOException;
 }
--- src/main/java/utils/view/ViewLoader.java
+++ src/main/java/utils/view/ViewLoader.java
@@ -56,7 +56,7 @@ public interface ViewLoader<S> {
      *
      * @param controller the controller to be used
      */
-    void createMemoView(GenericController memoControllerImpl);
+    void createMemoView(GenericController controller);
 
     /**
      * This method creates a dice view.
